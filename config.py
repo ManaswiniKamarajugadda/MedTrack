5ab719752c6c7c26fd98a586cefd23f56e80c111
ManaswiniKamarajugadda <manaswinikamarajugadda@gmail.com>
2026-04-14 15:41:07 +0530

    Change default AWS region to ap-south-1

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "change-me-in-production-please")
 # ─── AWS ──────────────────────────────────────────────────────────────────────
 AWS_ACCESS_KEY_ID     = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION            = os.getenv("AWS_REGION", "us-east-1")
+AWS_REGION            = os.getenv("AWS_REGION", "ap-south-1")
 
 # ─── DynamoDB table names ─────────────────────────────────────────────────────
 USERS_TABLE       = "medtrack_users"
